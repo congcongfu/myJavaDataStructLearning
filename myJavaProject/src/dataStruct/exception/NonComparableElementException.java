6413b69216526ac7e19599fc52c317a9192866e3
fudacong <fudacong@163.com>
2016-03-29 15:16:30 +0800

    change NonComparableElementException.java

--- myJavaProject/src/dataStruct/exception/NonComparableElementException.java
+++ myJavaProject/src/dataStruct/exception/NonComparableElementException.java
@@ -1,6 +1,9 @@
 package dataStruct.exception;
 
 public class NonComparableElementException extends RuntimeException {
+	/*Sets up this exception with an appropriate message
+	 * @param orderedList the name of orderedList
+	 * */
 	public NonComparableElementException(String orderedList){
 		super("The " + orderedList +" is empty");
 	}
