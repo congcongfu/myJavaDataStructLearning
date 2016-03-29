e9083356a8db9368975b7cc6ca4692d7be82ee9d
fudacong <fudacong@163.com>
2016-03-29 15:15:21 +0800

    change ElemenNotFoundException.java

--- myJavaProject/src/dataStruct/exception/ElemenNotFoundException.java
+++ myJavaProject/src/dataStruct/exception/ElemenNotFoundException.java
@@ -3,7 +3,7 @@ package dataStruct.exception;
 public class ElemenNotFoundException extends RuntimeException {
 	
 	/*Sets up this exception with an appropriate message
-	 * @param collection the name of collection
+	 * @param element the name of element
 	 * */
 	public ElemenNotFoundException(String element){
 		super("The " + element +" is empty!");
