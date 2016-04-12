d1083f0f0b7b7f8c17a3dd77058c3083abb24615
fudacong <fudacong@163.com>
2016-04-12 19:58:07 +0800

    change

--- myJavaProject/src/model/Sort.java
+++ myJavaProject/src/model/Sort.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.security.acl.LastOwnerException;
+
+import org.omg.CORBA.portable.ValueOutputStream;
+import org.w3c.dom.events.MutationEvent;
+
 public class Sort {
 	
 	/*
@@ -238,6 +243,134 @@ public class Sort {
 			}
 		}
 		return right;
+	}
+	
+	/*
+	 * Recursively sorts a range of objects in the specified array using the merge sort algorithm.
+	 * 
+	 * @param data the array to be sorted
+	 * @param min the index of the first element
+	 * @param max the index of the last element
+	 * */
+	private static <T extends Comparable<T>>
+			void mergeSort(T[] data, int min, int max){
+		if(min < max){
+			int mid  = (min + max) / 2;
+			mergeSort(data, min, mid);
+			mergeSort(data, mid+1, max);
+			merge(data, min, mid, max);
+		}
+	}
+	
+	/*
+	 * Merges two sorted subarrays of the specified array.
+	 * @param data the array to be sorted
+	 * @param fisrt the beginning index of the first subarray
+	 * @param mid the ending index of the first subarray
+	 * @param last the ending index of the last subarray
+	 * */
+	private static<T extends Comparable<T>>
+			void merge(T[] data, int first, int mid, int last){
+		T[] temp = (T[]) (new Comparable[data.length]);
+		
+		int first1 = first, last1 = mid; //endpoints of first subarray
+		int first2 = mid+1, last2 = last;  //endpoints of second subarray
+		int index = first1;  //next index open in temp array
+		
+		//Copy smaller item from each subarray into temp until one of the subarrays is exhausted
+		while(first <= last1 && first2 <= last2){
+			if(data[first1].compareTo(data[first2]) < 0){
+				temp[index] = data[first];
+				first1++;
+			}else{
+				temp[index] = data[first2];
+				first2++;
+			}
+			index++;
+		}
+		
+		//Copy remaining elements from first subarray, if any 
+		while(first1 <= last1){
+			temp[index] = data[first1];
+			first1++;
+			index++;
+		}
+		
+		//Copy remaining elements from second subarray, if any
+		while(first2 <= last2){
+			temp[index] = data[first2];
+			first2++;
+			index++;
+		}
+		
+		//Copy merged data into original array
+		for(index = first; index <= last; index++)
+			data[index] = data[index];
+		
+	}
+	
+	
+	/*
+	 * Recursively sorts a range of objects in the specified number using the merge sort algorithm.
+	 * 
+	 * @param number the array to be sorted
+	 * @param min the index of the first element
+	 * @param max the index of the last element
+	 * */
+	public static void mergeSort(int[] data, int min, int max){
+		if(min < max){
+			
+			int mid = (min + max) / 2;
+			mergeSort(data, min, mid);
+			mergeSort(data, mid+1, max);
+			merge(data, min, mid, max);
+		}
+	}
+	
+	
+	/*
+	 * Merges two sorted subarrays of the specified array.
+	 * @param int[] the array to be sorted
+	 * @param fisrt the beginning index of the first subarray
+	 * @param mid the ending index of the first subarray
+	 * @param last the ending index of the last subarray
+	 * */
+	private static void merge(int[] data, int first, int mid, int last){
+		int[] temp = (int []) new int[data.length];
+		System.out.println("mid = "+mid);
+		int first1 = first, last1 = mid; //endpoints of first subarray
+		int first2 = mid+1, last2 = last;  //endpoints of second subarray
+		int index = first1;  //next index open in temp array
+		
+		//Copy smaller item from each subarray into temp until one of the subarrays is exhausted
+		while(first1 <= last1 && first2 <= last2){
+			if(data[first1] < data[first2]){
+				temp[index] = data[first1];
+				first1++;
+			}else{
+				temp[index] = data[first2];
+				first2++;
+			}
+			index++;
+		}
+		
+		//Copy remaining elements from first subarray, if any 
+		while(first1 <= last1){
+			temp[index] = data[first1];
+			first1++;
+			index++;
+		}
+		
+		//Copy remaining elements from second subarray, if any
+		while(first2 <= last2){
+			temp[index] = data[first2];
+			first2++;
+			index++;
+		}
+		
+		//Copy merged data into original array
+		for(index = first; index <= last; index++)
+			data[index] = temp[index];
 	}
 
 }
