a1363bd429c24bc00b29ee2247be58935e0fe867
fudacong <fudacong@163.com>
2016-04-28 14:43:10 +0800

    add HeapADT

---
+++ myJavaProject/src/interfaces/HeapADT.java
@@ -0,0 +1,24 @@
+package interfaces;
+
+public interface HeapADT<T> extends BinaryTreeADT<T>{
+		
+	/**
+	 * Adds the specified object to this heap
+	 * 
+	 * @param obj the element to be added to the heap
+	 * */
+	public void addElement(T obj);
+	
+	/**
+	 * Remove element with the lowest value from this heap
+	 * @return the element with the lowest value from the heap
+	 * */
+	public T removeMin();
+	
+	/**
+	 * Returns a reference to the element with the lowest value in this heap
+	 * 
+	 * @return a reference to the element with the lowest value in the heap
+	 * */
+	public T findMin();
+}
