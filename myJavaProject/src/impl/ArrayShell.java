b3cb8adc50c0d5796b07c6c5984acba799929dd2
fudacong <fudacong@163.com>
2016-05-03 20:14:28 +0800

    add ArrayShell.java

---
+++ myJavaProject/src/impl/ArrayShell.java
@@ -0,0 +1,117 @@
+package impl;
+
+public class ArrayShell {
+
+	private int[] theArray;
+	private int count;
+	
+	/**
+	 * Create a new empty ArrayShell
+	 * @param int number size of the ArrayShell
+	 * */
+	public ArrayShell(int max){
+		theArray = new int[max];
+		count = 0;
+	}
+	
+	/***
+	 * Insert a number into the Array;
+	 * @param long value to be insert into the Array
+	 * */
+	public void insert(int value){
+		theArray[count] = value;
+		count++;
+	}
+	
+	/**
+	 * Displays array contents
+	 * */
+	public void display(){
+		System.out.println("A=");
+		for(int j = 0; j < count; j++)       
+			System.out.println(theArray[j] + " ");
+		System.out.println("");
+	}
+	
+	/**
+	 * Sort the array in an order;
+	 * */
+	public void shellSort(){
+		int inner, outer;
+		int temp;
+		
+		int h = 1;      //find the initial value of h
+		while(h < count/ 3)
+			h= h*3 +1;
+		
+		while(h > 0){     
+			for(outer = h; outer < count; outer++){
+				temp = theArray[outer];
+				inner = outer;
+				
+				while(inner > h -1 && theArray[inner - h] >= temp){
+					theArray[inner] = theArray[inner - h];
+					inner -= h;
+				}
+				theArray[inner] = temp;
+			}//end for;
+			h = (h -1) / 3; //decrease h
+		}  //end shellSort()
+	}
+	
+	/**
+	 * Returns a string to represents the array
+	 * @returns a string to represents the array
+	 * */
+	public String toString(){
+		String result = "{";
+		for(int i = 0; i < count; i++){
+			if(i < count -1)
+				result += theArray[i] + ",";
+			else 
+				result += theArray[i];
+		}
+		result += "}";
+		return result;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- myJavaProject/src/test/Test.java
+++ myJavaProject/src/test/Test.java
@@ -1,10 +1,12 @@
 package test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
@@ -15,11 +17,11 @@ public class Test {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 		
-		int [] array = { 7,4,3};
-		Sort.mergeSort(array, 0, array.length-1);
-		for(int i=0; i< array.length; i++){
-			System.out.println(array[i]);
-		}
+//		int [] array = { 7,4,3};
+//		Sort.mergeSort(array, 0, array.length-1);
+//		for(int i=0; i< array.length; i++){
+//			System.out.println(array[i]);
+//		}
 	}
 	public static int sum(int num){
 		int result;
@@ -30,5 +32,30 @@ public class Test {
 		return result;
 		
 	}
-
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
