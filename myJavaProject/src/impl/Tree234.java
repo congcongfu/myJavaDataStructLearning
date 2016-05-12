0746a3f61500f5ab17b659e5a7e695972b0f215c
fudacong <fudacong@163.com>
2016-05-12 17:02:21 +0800

    change Tree234.java

--- myJavaProject/src/impl/Tree234.java
+++ myJavaProject/src/impl/Tree234.java
@@ -19,46 +19,59 @@ public class Tree234 {
 		}
 	}
 
+	/**
+	 * Gets appropriate child of node during search for value
+	 * */
 	private Node_3 getNextChild(Node_3 curNode, int key) {
-		// TODO Auto-generated method stub
-		return null;
+		int i;
+		int numItems = curNode.getNumItems();
+		for (i = 0; i < numItems; i++) {
+			if( key < curNode.getItem(i).dData){
+				return curNode.getChild(i);    //return left child
+			}
+		}
+		
+		return curNode.getChild(i);
 	}
 	
 	/**
-	 * 
+	 * Inserts the a specified value into the tree
+	 * @param int dValue to be inserted into the tree
 	 * */
 	public void insert(int dValue){
 		Node_3 curNode = root;
+		Node_3 nextChild;
 		DataItem tempItem = new DataItem(dValue);
-		
 		while(true){
 			if(curNode.isFull()){
-				spilt(curNode);
-				curNode = getNextChild(curNode, dValue);
-			}   //end if(node is full)
-			
-			else if(curNode.isLeaf())    //if node is leaf
+				split(curNode);
+				nextChild = getNextChild(root, dValue);
+				if(nextChild != null)
+						curNode = nextChild;
+			}else if(curNode.isLeaf()){    //if node is leaf
 				break;                  // go insert
-			else
-				curNode = getNextChild(curNode, dValue);
+			}else{
+					curNode = getNextChild(curNode, dValue);
+				} 
 		}
-		curNode.insertItem(tempItem);        //insert new DataItem
+		curNode.insertItem(tempItem);   //insert new DataItem
+		
+		    
 	}
 
 	/**
-	 * Removes items from this node 
-	 * remove children from this node
+	 * Splits the items if it is full 
 	 * */
-	private void spilt(Node_3 thisNode) {
+	private void split(Node_3 thisNode) {
+
 		DataItem itemB, itemC;
 		Node_3 parent, child2, child3;
 		int itemIndex;
 		
 		itemC = thisNode.removeItem();    
 		itemB = thisNode.removeItem();
-		child2 = thisNode.disconnectChid(2);
-		child3 = thisNode.disconnectChid(3);
-		
+		child2 = thisNode.disconnectChild(2);
+		child3 = thisNode.disconnectChild(3);
 		Node_3 newRight = new Node_3();
 		
 		if(thisNode == root){
@@ -72,14 +85,38 @@ public class Tree234 {
 		int n = parent.getNumItems();
 		
 		for(int j = n-1; j > itemIndex; j--){
-			Node_3 temp = parent.disconnectChid(j);
+			Node_3 temp = parent.disconnectChild(j);
 			parent.connectChild(j+1, temp);
 		}
-		
+
 		parent.connectChild(itemIndex+1, newRight);
 		newRight.insertItem(itemC);
 		newRight.connectChild(0, child2);
 		newRight.connectChild(1, child3);
+	}
+	
+	public void displayTree(){
+		recDisplayTree(root, 0, 0);
+	}
+
+	/**
+	 * display the tree
+	 * */
+	private void recDisplayTree(Node_3 thisNode, int level, int childNumber) {
+
+		System.out.println("level="+level+" child="+childNumber+" ");
+		thisNode.diplayNode();
+		
+		//call ourselves for each child of this node
+		int numItems = thisNode.getNumItems();
+		for(int i = 0; i < numItems+1; i++){
+			Node_3 nextNode = thisNode.getChild(i);
+			if(nextNode != null){
+				recDisplayTree(nextNode, level+1, i);
+			}else{
+				return;
+			}
+		}
 	}
 }
 
