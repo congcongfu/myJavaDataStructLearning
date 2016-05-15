4071ea729bd84f3dbb3bdd5fb328ed0c7f4988af
congcongfu <1246532936@qq.com>
2016-05-15 19:39:08 +0800

    add Link.java

---
+++ myJavaProject/src/model/Link.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Link {
+	private int dData;
+	public Link next;
+	
+	public Link(int data){
+		dData = data;
+	}
+	
+	public int getKey(){
+		return dData;
+	}
+	
+	public void displayLink(){
+		System.out.println(dData + " ");
+	}
+
+}
